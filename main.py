f6602c8edd2fc9d519c22e17a93e9d2d8a087276
ArjunaRus <arjuna_na@mail.ru>
2024-06-23 17:17:33 +0300

    Use set instead list

--- main.py
+++ main.py
@@ -24,15 +24,16 @@ def sieve_of_eratosthenes_luzin(n: int) -> int:
         Функция печатает простые числа в интервале от 2 до n.
         Реализация А.Лузина.
     """
-    primes = list(range(2, n))
-    k_max_pereimenovat = 0
-    for k in primes:
+    not_primes = set()
+    for k in range(2, n+1):
+        if k in not_primes:
+            continue
+
         print(k, end=' ')
+
         for i in range(k, n // k + 1):
-            if i*k in primes:
-                primes.remove(i*k)
-                k_max_pereimenovat = k
-    return k_max_pereimenovat
+            not_primes.add(i*k)
+    return 0
 
 
 def main():
