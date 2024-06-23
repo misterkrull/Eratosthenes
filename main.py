84f8456c05f09d170408ddf07f2e858fcbfd15c7
Yaroslav Krull <krulll@yandex.ru>
2024-06-23 18:26:57 +0500

    Second version of program Algorithm is slow (with deleting elements of list) Code style is good

--- main.py
+++ main.py
@@ -1,38 +1,64 @@
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+﻿"""
+    Данный модуль реализует алгоритм решета Эратосфена
+    Пользователь в терминале вводит число n, а программа выводит простые числа от 2 до n
+"""
+# TODO: сделать функцию Sieve_of_Eratosthenes, которая выводит список простых чисел
+# TODO: придумать называние переменной p_max
+#  многострочный to_do: нужно всего-лишь сделать отступ в один пробел))
 
-# class numb:
-#    isprime : bool = True
 
-def Sieve_of_Eratosthenes():
+def sieve_of_eratosthenes(max_num: int) -> tuple[list[int], int]:
+    """
+        Функция принимает один аргумент max_num и возвращает:
+            - список простых чисел между 2 и max_num, вычисленный с помощью алгоритма
+             решета Эратосфена;
+            - наибольшее простое число, кратные которому вычёркивались из списка
+             согласно алгоритму Эратосфена.
+    """
+    list_of_primes: list[int] = list(range(2, max_num+1))
+    p_max: int = 0
+
+    for p in list_of_primes:
+        if p**2 >= max_num:
+            break
+        p_max: int = p
+        q: int = p
+        while q*p <= max_num:
+            if q*p in list_of_primes:
+                list_of_primes.remove(q*p)
+            q = q + 1
+
+    return list_of_primes, p_max
+
+
+def main():
+    """
+            Эта функция осуществляет операции ввода и вывода,
+            а также вызывает функцию sieve_of_eratosthenes
+    """
+
     print()
-    print("Доброго времени суток! Это - первая нормальная полностью мной написанная на Python'e программа!")
-    print("Это программа реализует алгоритм решета Эратосфена по выписыванию всех простых чисел в диапазане от 2 до n включительно")
+    print("Доброго времени суток! Это - первая нормальная полностью мной написанная на Python'e "
+          "программа!")
+    print("Это программа реализует алгоритм решета Эратосфена по выписыванию всех простых чисел "
+          "в диапазоне от 2 до n включительно")
     print("(число n задаётся пользователем)")
     print()
 
-    n = int(input("Введите n : "))
-        # тут ннада сделать try :)
+    while True:
+        try:
+            n: int = int(input("Введите n : "))
+            break
+        except ValueError:
+            print("Нужно ввести целое число, а вы ввели что-то другое. Попробуйте ещё раз")
 
-    is_prime = [1 for i in range(n+1)]
-    out_str = f"Список простых чисел от 2 до {n} :"
-    k_max = 0
-
-    for k in range(2, n+1):
-        if (is_prime[k]):
-            out_str = out_str + f" {k}"
-            i = k
-            while (i*k <= n):
-                if is_prime[i*k] == 1:
-                    k_max = max(k_max, k)
-                is_prime[i*k] = 0
-                i = i + 1
+    list_of_primes, p_max = sieve_of_eratosthenes(n)
 
     print()
-    print(out_str)
-    print(f"k_max = {k_max}")
+    print(f"Список простых чисел в диапазоне от 2 до {n} : ", *list_of_primes)
+    print(f"p_max = {p_max}")
     input()
 
-    # Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    Sieve_of_Eratosthenes()
+    main()
